d9047c8b3f1a5ed7f3b63c512bc6c269a56de616
Ben McCarrick <b.mccarrick4326@gmail.com>
2024-05-24 11:17:09 +0100

    adjusted readme and added styling for name inputs

--- DartsScorer/Components/HomePage.js
+++ DartsScorer/Components/HomePage.js
@@ -6,8 +6,8 @@ import { useNavigation } from '@react-navigation/native';
 const HomePage = () => {
 
 
-    const [nameTextPlayer1, setNameTextPlayer1] = useState("Player 1");
-    const [nameTextPlayer2, setNameTextPlayer2] = useState("Player 2");
+    const [nameTextPlayer1, setNameTextPlayer1] = useState("");
+    const [nameTextPlayer2, setNameTextPlayer2] = useState("");
     const navigation = useNavigation();
     
 
@@ -18,17 +18,28 @@ const HomePage = () => {
         <Text style={styles.textStyle}>For a multiplayer game:</Text>
         <TextInput
           style={styles.textStyle}
-          placeholder='Player 1'
+          placeholder='Enter Player 1 name'
+          placeholderTextColor='blue'
           onChangeText={(newNameTextPlayer1) => setNameTextPlayer1(newNameTextPlayer1)}
-          defaultValue='Player 1'
         />
         <TextInput
           style={styles.textStyle}
-          placeholder='Player 2'
+          placeholder='Enter Player 2 name'
+          placeholderTextColor='blue'
           onChangeText={(newNameTextPlayer2) => setNameTextPlayer2(newNameTextPlayer2)}
-          defaultValue='Player 2'
         />
         <Pressable
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed
+              ? "rgb(210, 230, 255)"
+              : "black",
+            borderRadius: 20,
+            padding: 9,
+            elevation: 0,
+            marginBottom: 10,
+          },
+        ]}
               onPress={() => {navigation.navigate("Multiplayer Game", {
                 player1Name: nameTextPlayer1,
                 player2Name: nameTextPlayer2,
@@ -39,6 +50,17 @@ const HomePage = () => {
           <View style= {styles.singlePlayer}>
       <Text style={styles.textStyle}>For a single player game:  </Text>
       <Pressable
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed
+            ? "rgb(210, 230, 255)"
+            : "black",
+          borderRadius: 20,
+          padding: 5,
+          elevation: 5,
+          marginBottom: 15,
+        },
+      ]}
               onPress={() => {navigation.navigate("Single Player Game")}}>
               <Text style={styles.buttonStyle}>PLAY DARTS!</Text>
             </Pressable>
@@ -59,6 +81,15 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 25,
     textAlign: 'center',
+    borderStartWidth : 2,
+    borderEndWidth : 2,
+    borderTopWidth : 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderBottomWidth : 2,
+    borderColor: 'white',
+    padding: 10,
+    marginBottom: 10,
   },
   buttonStyle: {
     color: 'red',
@@ -77,12 +108,12 @@ const styles = StyleSheet.create({
 
   },
   singlePlayer: {
-    marginTop: 190,
-    marginBottom: 190,
+    marginTop: 150,
+    marginBottom: 170,
   },
   multiPlayer: {
     marginTop: 40,
-    marginBottom: 220
+    marginBottom: 160
   },
 });
 
